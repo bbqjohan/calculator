e9cdfaf772e804d48acc46d66b2d48c532406f29
Markus Sjöqvist <korvkod@gmail.com>
2025-09-26 18:05:57 +0200

    Now accepts more than two numbers
    - Refactoring console output.
    - Fixed bugs where the scanner did not read next line properly.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Calculator {
     private final Scanner scanner;
-    private double result;
+    private double total;
 
+    /**
+     * Represents the different mathematical operators this application
+     * supports.
+     */
     private enum Operator {
         ADD,
         SUBTRACT,
@@ -13,44 +18,38 @@ public class Calculator {
 
     public Calculator() {
         scanner = new Scanner(System.in);
-        result = 0;
+        total = 0;
     }
 
     /**
      * Start the calculator. The calculator has a precision of 4 decimals.
      */
     public void run() {
-        System.out.println("Välkommen till kalkylatorn!");
-        System.out.println("Lägg ihop två nummer med en av följande operatorer: +, -, / eller *");
-        System.out.println("Ni kan när som helst skriva \"exit\" för att avsluta programmet");
+        System.out.println("\nVälkommen till kalkylatorn!");
+        System.out.println("Välj ett räknesätt och gör din kalkyl!");
+        System.out.println("När ni skriver in nummer, separera dom med mellanslag: 1 2 3");
+        System.out.println("Ni kan när som helst skriva \"exit\" för att avsluta programmet.");
 
         while (true) {
-            System.out.print("Nummer 1: ");
-            double num1 = nextNumber();
-
-            System.out.print("Nummer 2: ");
-            double num2 = nextNumber();
-
-            System.out.print("Operator (+, -, /, *): ");
+            System.out.println(); // Empty line for formatting.
             Operator operator = nextOperator();
 
             switch (operator) {
                 case ADD:
-                    result = num1 + num2;
+                    total = addNumbers(nextNumbers());
                     break;
                 case SUBTRACT:
-                    result = num1 - num2;
+                    total = subtractNumbers(nextNumbers());
                     break;
                 case MULTIPLY:
-                    result = Math.floor((num1 * num2) * 10000) / 10000;
+                    total = multiplyNumbers(nextNumbers());
                     break;
                 case DIVIDE:
-                    result = Math.floor((num1 / num2) * 10000) / 10000;
+                    total = divideNumbers(nextNumbers());
                     break;
             }
 
-            System.out.println("Resultat: " + result);
-            System.out.println("Vill ni fortsätta? (Y/N)");
+            System.out.println("Resultat: " + total);
 
             if (!promptKeepGoing()) {
                 System.exit(0);
@@ -59,19 +58,128 @@ public class Calculator {
     }
 
     /**
-     * Prompts user for a number. If a number is not provided, the user will be
-     * prompted to try again until they succeed, or exits the application.
+     * Performs addition on a list of numbers.
      *
-     * @return The user input as a double.
+     * @param numbers The numbers to add together.
+     * @return The result of adding all numbers together.
      */
-    private double nextNumber() {
-        while (!scanner.hasNextDouble()) {
-            maybeExit(scanner.next());
-            System.out.println(" Det där är inte ett nummer! >:(");
-            scanner.nextLine();
+    private double addNumbers(double[] numbers) {
+        return Arrays.stream(numbers).sum();
+    }
+
+    /**
+     * Performs sequential subtraction on an array of numbers.
+     *
+     * @param numbers The numbers to perform subtraction on.
+     * @return the result of sequential subtraction starting from the first
+     * number, or 0 if the array is empty
+     */
+    private double subtractNumbers(double[] numbers) {
+        double result = 0;
+
+        if (numbers.length > 0) {
+            result = numbers[0];
         }
 
-        return scanner.nextDouble();
+        for (int i = 1; i < numbers.length; i++) {
+            result -= numbers[i];
+        }
+
+        return result;
+    }
+
+    /**
+     * Multiplies a list of numbers.
+     *
+     * @param numbers The numbers to multiply together.
+     * @return The result of the multiplication.
+     */
+    private double multiplyNumbers(double[] numbers) {
+        double result = 0;
+
+        if (numbers.length > 0) {
+            result = numbers[0];
+        }
+
+        for (int i = 1; i < numbers.length; i++) {
+            result *= numbers[i];
+        }
+
+        return toPrecision(result);
+    }
+
+    /**
+     * Divides a list of numbers.
+     *
+     * @param numbers The numbers to divide together.
+     * @return The result of the division.
+     */
+    private double divideNumbers(double[] numbers) {
+        double result = 0;
+
+        if (numbers.length > 0) {
+            result = numbers[0];
+        }
+
+        for (int i = 1; i < numbers.length; i++) {
+            result /= numbers[i];
+        }
+
+        return toPrecision(result);
+    }
+
+    /**
+     * Takes a number and cuts of any decimals after the fourth.
+     *
+     * @param num The number to cut down to precision.
+     * @return The result of cutting of any decimals after the fourth.
+     */
+    private double toPrecision(double num) {
+        return Math.floor((num) * 10000) / 10000;
+    }
+
+    /**
+     * Prompts the user to enter new numbers in the console. Each number must
+     * be separated by a space. The user will continue to be prompted until
+     * they either exit the application or give the correct input.
+     *
+     * @return A list of numbers parsed from user input.
+     */
+    private double[] nextNumbers() {
+        double[] numbers = null;
+
+        while (true) {
+            System.out.print("Skriv in nummer: ");
+            String input = scanner.nextLine();
+
+            try {
+                numbers = parseInput(input);
+                break;
+            } catch (Exception e) {
+                // Do nothing. The loop will continue and prompt the user until
+                // the input is correct.
+                maybeExit(input);
+            }
+        }
+
+        return numbers;
+    }
+
+    /**
+     * Parses user input into a double[].
+     *
+     * @param line Input to parse.
+     * @return A list of double values that was parsed from the input.
+     */
+    private double[] parseInput(String line) {
+        String[] splits = line.trim().split("\\s+");
+        double[] result = new double[splits.length];
+
+        for (int i = 0; i < splits.length; i++) {
+            result[i] = Double.parseDouble(splits[i]);
+        }
+
+        return result;
     }
 
     /**
@@ -82,21 +190,32 @@ public class Calculator {
      * @return The operator as an {@link Operator}.
      */
     private Operator nextOperator() {
-        while (true) {
-            String input = scanner.next();
+        Operator operator = null;
+
+        while (operator == null) {
+            System.out.print("Välj en operator (+, -, /, *): ");
+            String input = scanner.nextLine().trim();
 
             switch (input) {
-                case "+": return Operator.ADD;
-                case "-": return Operator.SUBTRACT;
-                case "*": return Operator.MULTIPLY;
-                case "/": return Operator.DIVIDE;
+                case "+":
+                    operator = Operator.ADD;
+                    break;
+                case "-":
+                    operator = Operator.SUBTRACT;
+                    break;
+                case "*":
+                    operator = Operator.MULTIPLY;
+                    break;
+                case "/":
+                    operator = Operator.DIVIDE;
+                    break;
                 default: {
                     maybeExit(input);
-                    System.out.println("Det där är inte en giltig operator! (+, -, /, *)");
-                    scanner.nextLine();
                 }
             }
         }
+
+        return operator;
     }
 
     /**
@@ -109,15 +228,16 @@ public class Calculator {
      */
     private boolean promptKeepGoing() {
         while (true) {
-            String input = scanner.next();
+            System.out.print("Vill ni fortsätta? (Y/N): ");
+            String input = scanner.nextLine().trim().toLowerCase();
 
-            switch (input.toLowerCase()) {
-                case "n": return false;
-                case "y": return true;
+            switch (input) {
+                case "n":
+                    return false;
+                case "y":
+                    return true;
                 default: {
                     maybeExit(input);
-                    System.out.println("Vänligen skriv ett giltigt svar: Y eller N");
-                    scanner.nextLine();
                 }
             }
         }
